8109997e98ec89a8c1fc8a3f82b0143faee0ddcf
LepeshenkovNick <Lepeshenkov.nick@gmail.com>
2022-04-26 17:37:17 +0300

    hw6

---
+++ src/js/task-07.js
@@ -0,0 +1,10 @@
+const textEl = document.getElementById('text');
+const inputEl = document.getElementById('font-size-control');
+
+const onChangeText = event => {
+    const size = inputEl.value;
+
+    textEl.style.fontSize = size + 'px';
+};
+
+inputEl.addEventListener('input', onChangeText);
---
+++ src/js/task-08.js
@@ -0,0 +1,26 @@
+const formRef = document.querySelector(`.login-form`);
+const formBtn = document.querySelector(`button`);
+
+formRef.addEventListener(`submit`, onSubmitForm);
+
+function onSubmitForm(event) {
+  event.preventDefault();
+
+  const formElements = event.currentTarget.elements;
+
+  const mail = formElements.email.value;
+  const password = formElements.password.value;
+
+  const formData = {
+    mail,
+    password,
+  };
+
+  if (formData.mail === '' || formData.password === '') {
+    window.alert('Все поля должны быть заполнены!');
+    return;
+  } else {
+    console.log(formData);
+  }
+  formRef.reset();
+}
